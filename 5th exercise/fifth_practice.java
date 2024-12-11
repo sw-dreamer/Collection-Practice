2acb696f53064b288abbd7167d98b853b3ce9a69
sw <sw.woo.dreamer@gmail.com>
2024-12-11 16:27:11 +0900

    Update fifth_practice.java

--- 5th exercise/fifth_practice.java
+++ 5th exercise/fifth_practice.java
@@ -3,22 +3,37 @@ package collection_package;
 import java.util.*;
 
 class StudentInfo {
-	String student_name;
-	String department_information;
-	String student_number;
-	double student_score;
+	private String student_name;
+	private String department_information;
+	private int student_number;
+	private double student_score;
 
-	public StudentInfo(String student_name, String department_information, String student_number,
-			double student_score) {
+	public StudentInfo(String student_name, String department_information, int student_number, double student_score) {
 		this.student_name = student_name;
 		this.department_information = department_information;
-		this.student_number = student_number;
-		this.student_score = student_score;
+		this.setStudent_number(student_number);
+		this.setStudent_score(student_score);
 	}
 
 	public void printStudentInfo() {
-		System.out.println("이름: " + student_name + ", 학과: " + department_information + ", 학번: " + student_number
-				+ ", 학점 평균: " + student_score);
+		System.out.println("이름: " + student_name + ", 학과: " + department_information + ", 학번: " + getStudent_number()
+				+ ", 학점 평균: " + getStudent_score());
+	}
+
+	public int getStudent_number() {
+		return student_number;
+	}
+
+	public void setStudent_number(int student_number) {
+		this.student_number = student_number;
+	}
+
+	public double getStudent_score() {
+		return student_score;
+	}
+
+	public void setStudent_score(double student_score) {
+		this.student_score = student_score;
 	}
 }
 
@@ -45,21 +60,26 @@ public class fifth_practice {
 					System.out.print("학과: ");
 					String department_information = scanner.nextLine();
 
-					String student_number = null;
+					int student_number = 0;
 					while (true) {
 						System.out.print("학번: ");
-						student_number = scanner.next();
-						isduplicated = false;
-						for (StudentInfo student : insert_student) {
-							if (student.student_number.equals(student_number)) {
-								isduplicated = true;
-								System.out.println("이미 존재하는 학번입니다. 다른 학번을 입력해주세요.");
+						try {
+							student_number = Integer.parseInt(scanner.next());
+							isduplicated = false;
+							for (StudentInfo student : insert_student) {
+								if (student.getStudent_number() == student_number) {
+									isduplicated = true;
+									System.out.println("이미 존재하는 학번입니다. 다른 학번을 입력해주세요.");
+									break;
+								}
+							}
+							if (isduplicated == false) {
 								break;
 							}
+						} catch (NumberFormatException e) {
+							System.out.println("숫자로 입력하세요");
 						}
-						if (isduplicated == false) {
-							break;
-						}
+
 					}
 					scanner.nextLine();
 
@@ -102,7 +122,7 @@ public class fifth_practice {
 		double sum = 0.0;
 		double result = 0.0;
 		for (StudentInfo student : insert_student) {
-			sum += student.student_score;
+			sum += student.getStudent_score();
 		}
 		result = sum / number;
 		System.out.println("입력된 학생들의 학점 합은 " + sum + "이고 입력된 학생들의 학접 평균은 " + result + "입니다");
