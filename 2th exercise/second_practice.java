af2a0d2a478efbe1baf718e8729d57600b783a35
sw <sw.woo.dreamer@gmail.com>
2024-12-11 17:20:57 +0900

    Update second_practice.java

--- 2th exercise/second_practice.java
+++ 2th exercise/second_practice.java
@@ -2,6 +2,20 @@ package collection_package;
 
 import java.util.*;
 
+/**
+ * @packageName	: collection_package
+ * @fileName	: second_practice.java
+ * @author		: TJ
+ * @date		: 2024.12.11
+ * @description	: Scanner 클래스를 사용하여 6개 학점('A', 'B', 'C', 'D', 'F')을 문자로 입력받아 ArrayList에 저장하고,
+ * 		  ArrayList를 검색하여 학점을 점수(A=4.0, B=3.0, C=2.0, D=1.0, F=0)로 변환하여 평균을 출력하는 프로그램을 작성하라.
+
+ * ========================================================================================
+ * DATE					AUTHOR				NOTE
+ * ----------------------------------------------------------------------------------------
+ * 2024.12.11				SW WOO				최초 생성
+ * 
+ */
 public class second_practice {
 
 	public static void main(String[] args) {
